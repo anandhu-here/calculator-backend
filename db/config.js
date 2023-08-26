74ff218aba4cc62b850c9e564c9d97d5058e5719
Anandhu Satheesh <anandhusatheesh@Anandhus-MacBook-Air.local>
2023-08-26 18:47:26 +0100

    db production

--- db/config.js
+++ db/config.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: '127.0.0.1',
-  database: 'calculator',
-  password: 'blackbird',
+  host: 'calculator-db.cskrza2seurw.us-east-1.rds.amazonaws.com',
+  database: 'calculator-db',
+  password: 'calculator123',
   port: 5432, // Default PostgreSQL port
 });
 
